83c7fc7b06acca1ce9f6f18e74ec22e55f3a3b3d
David Bevin <dpbevin@gmail.com>
2013-09-16 23:00:52 +1000

    Resolves #10. Confirmed all parsers are indeed static.

--- JIRC/Internal/Json/Gen/CommentJsonGenerator.cs
+++ JIRC/Internal/Json/Gen/CommentJsonGenerator.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using JIRC.Domain;
+﻿using JIRC.Domain;
 
 using ServiceStack.Text;
 
--- JIRC/Internal/Json/Gen/VersionInputJsonGenerator.cs
+++ JIRC/Internal/Json/Gen/VersionInputJsonGenerator.cs
@@ -1,5 +1,4 @@
-﻿
-using System;
+﻿using System;
 
 using JIRC.Domain.Input;
 using JIRC.Extensions;
